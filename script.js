f03dd27573c6a1baa08dfc08780b8d6d0978752f
Sara <152891585+valsecchisara@users.noreply.github.com>
2024-07-09 15:12:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,126 +1,131 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger'); // Seleziona il bottone hamburger
-    const nav = document.querySelector('nav'); // Seleziona il menu di navigazione
-    const links = document.querySelectorAll('nav a[href^="#"]'); // Seleziona tutti i link di navigazione interni
-    const lightbox = document.getElementById('lightbox'); // Seleziona il lightbox
-    const lightboxImage = document.querySelector('.lightbox-content'); // Seleziona l'immagine del lightbox
-    const lightboxClose = document.querySelector('.lightbox-close'); // Seleziona il bottone per chiudere il lightbox
-    const lightboxPrev = document.querySelector('.lightbox-prev'); // Seleziona il bottone per l'immagine precedente
-    const lightboxNext = document.querySelector('.lightbox-next'); // Seleziona il bottone per l'immagine successiva
-    const images = document.querySelectorAll('.gallery-container img'); // Seleziona tutte le immagini della galleria
-    const apiKey = 'wNW9BXvENBTT4ZPZdzJlxxBcsq5w4WSA'; // API key per NYT (da nascondere)
-    const newsContainer = document.getElementById('news-articles'); // Seleziona il contenitore per le notizie
-    let currentIndex = 0; // Indice corrente per la navigazione del lightbox
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+    const links = document.querySelectorAll('nav a[href^="#"]');
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImage = document.querySelector('.lightbox-content');
+    const lightboxClose = document.querySelector('.lightbox-close');
+    const lightboxPrev = document.querySelector('.lightbox-prev');
+    const lightboxNext = document.querySelector('.lightbox-next');
+    const images = document.querySelectorAll('.gallery-container img');
+    const apiKey = 'wNW9BXvENBTT4ZPZdzJlxxBcsq5w4WSA';
+    const newsContainer = document.getElementById('news-articles');
+    let currentIndex = 0;
 
     function toggleNav() {
-        nav.style.display = (nav.style.display === 'block') ? 'none' : 'block'; // Alterna la visibilità del menu di navigazione
+        nav.style.display = (nav.style.display === 'block') ? 'none' : 'block';
     }
 
     function closeNav() {
         if (nav.style.display === 'block') {
-            nav.style.display = 'none'; // Chiude il menu di navigazione se è aperto
+            nav.style.display = 'none';
         }
     }
 
     function showLightbox(index) {
-        lightbox.style.display = 'block'; // Mostra il lightbox
-        lightboxImage.src = images[index].src; // Imposta l'immagine del lightbox
-        currentIndex = index; // Aggiorna l'indice corrente
+        lightbox.style.display = 'block';
+        lightboxImage.src = images[index].src;
+        currentIndex = index;
     }
 
     function closeLightbox() {
-        lightbox.style.display = 'none'; // Chiude il lightbox
+        lightbox.style.display = 'none';
     }
 
     function changeImage(step) {
-        currentIndex += step; // Cambia l'indice corrente
+        currentIndex += step;
         if (currentIndex >= images.length) {
-            currentIndex = 0; // Torna alla prima immagine se l'indice supera la lunghezza dell'array
+            currentIndex = 0;
         } else if (currentIndex < 0) {
-            currentIndex = images.length - 1; // Torna all'ultima immagine se l'indice è negativo
+            currentIndex = images.length - 1;
         }
-        showLightbox(currentIndex); // Mostra l'immagine aggiornata nel lightbox
+        showLightbox(currentIndex);
     }
 
     hamburger.addEventListener('click', function(event) {
-        toggleNav(); // Alterna la visibilità del menu di navigazione
-        event.stopPropagation(); // Ferma la propagazione dell'evento
+        toggleNav();
+        event.stopPropagation();
     });
 
-    document.body.addEventListener('click', closeNav, true); // Chiude il menu di navigazione quando si clicca fuori
+    document.body.addEventListener('click', closeNav, true);
 
     links.forEach(link => {
         link.addEventListener('click', function(event) {
-            event.preventDefault(); // Previene il comportamento predefinito del link
-            closeNav(); // Chiude il menu di navigazione
-            const targetId = this.getAttribute('href'); // Ottiene l'ID della sezione di destinazione
-            const targetSection = document.querySelector(targetId); // Seleziona la sezione di destinazione
+            event.preventDefault();
+            closeNav();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
             if (targetSection) {
-                const headerHeight = document.querySelector('.header').offsetHeight; // Altezza dell'header
-                const targetRect = targetSection.getBoundingClientRect(); // Coordinate della sezione di destinazione
-                const topOffset = targetRect.top + window.pageYOffset - headerHeight; // Calcola la posizione di scorrimento
-                window.scrollTo({ top: topOffset, behavior: 'smooth' }); // Scorre alla sezione di destinazione
+                const headerHeight = document.querySelector('.header').offsetHeight;
+                const targetRect = targetSection.getBoundingClientRect();
+                const topOffset = targetRect.top + window.pageYOffset - headerHeight;
+                window.scrollTo({ top: topOffset, behavior: 'smooth' });
             }
         });
     });
 
     images.forEach((img, index) => {
-        img.addEventListener('click', () => showLightbox(index)); // Mostra il lightbox quando si clicca su un'immagine
+        img.addEventListener('click', () => showLightbox(index));
     });
 
     lightboxPrev.addEventListener('click', function(event) {
-        changeImage(-1); // Cambia l'immagine precedente
-        event.stopPropagation(); // Ferma la propagazione dell'evento
+        changeImage(-1);
+        event.stopPropagation();
     });
 
     lightboxNext.addEventListener('click', function(event) {
-        changeImage(1); // Cambia l'immagine successiva
-        event.stopPropagation(); // Ferma la propagazione dell'evento
+        changeImage(1);
+        event.stopPropagation();
     });
 
     lightbox.addEventListener('click', function(event) {
         if (event.target === lightbox) {
-            closeLightbox(); // Chiude il lightbox se si clicca fuori dall'immagine
+            closeLightbox();
         }
     });
 
     lightboxClose.addEventListener('click', function(event) {
-        closeLightbox(); // Chiude il lightbox
-        event.stopPropagation(); // Ferma la propagazione dell'evento
+        closeLightbox();
+        event.stopPropagation();
     });
 
     function filterGallery(season) {
         const items = document.querySelectorAll('.gallery-container .gallery-item');
         items.forEach(item => {
-            // Verifica se l'elemento appartiene alla sezione di moda anziché alla sezione notizie
             if (item.closest('#gallery')) {
                 item.style.display = (season === 'all' || item.classList.contains(season)) ? 'block' : 'none';
             }
         });
     }
-    
 
     fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:(%22Fashion%22%20%22Style%22)&sort=newest&api-key=${apiKey}`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(data => {
-            data.response.docs.forEach(article => {
-                if (article.multimedia.length > 0) {
-                    const articleImg = `https://www.nytimes.com/${article.multimedia[0].url}`;
-                    const newsItem = document.createElement('div');
-                    newsItem.className = 'gallery-item'; // Usando lo stesso stile della galleria di immagini
-                    newsItem.innerHTML = `
-                        <img src="${articleImg}" alt="${article.headline.main}">
-                        <div class="news-text">
-                            <h3>${article.headline.main}</h3>
-                            <p>${article.snippet}</p>
-                            <a href="${article.web_url}" target="_blank">Leggi di più</a>
-                        </div>
-                    `;
-                    newsContainer.appendChild(newsItem);
-                }
-            });
+            if (data.response && data.response.docs) {
+                data.response.docs.forEach(article => {
+                    if (article.multimedia && article.multimedia.length > 0) {
+                        const articleImg = `https://www.nytimes.com/${article.multimedia[0].url}`;
+                        const newsItem = document.createElement('div');
+                        newsItem.className = 'news-item';
+                        newsItem.innerHTML = `
+                            <img src="${articleImg}" alt="${article.headline.main}">
+                            <div class="news-text">
+                                <h3>${article.headline.main}</h3>
+                                <p>${article.snippet}</p>
+                                <a href="${article.web_url}" target="_blank">Leggi di più</a>
+                            </div>
+                        `;
+                        newsContainer.appendChild(newsItem);
+                    }
+                });
+            }
         })
         .catch(error => console.error('Errore nel caricamento delle notizie:', error));
 
-    window.filterGallery = filterGallery; // Rende la funzione filterGallery accessibile globalmente
+    window.filterGallery = filterGallery;
 });
